6b584b3c58d49c0d62417643216365866787712a
Sidnei <sidneisonic@hotmail.com>
2024-03-14 12:59:37 -0300

    Aula 36 renderizando condicional cm useState

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,27 @@ import { useState } from "react";
 
 const App = () => {
   const [valor, setValor] = useState(0);
+  const [logged, setLogged] = useState(false);
 
   const handleClick = () => {
     setValor(valor + 1);
+  }
+
+  const handleClick2 = () => {
+    setLogged(!logged);
   }
 
   return (
     <div>
       <p>Você clicou {valor} vezes</p>
       <button onClick={handleClick}>Clique em mim</button>
+
+      <div>
+        {logged && <p>Usuario Logado</p>}
+        <button onClick={handleClick2}>
+          {logged ? 'Logout' : 'Login'}
+        </button>
+      </div>
     </div>
   );
 }

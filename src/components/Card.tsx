fed6755b260c1c638ef0237bd22124ede35afd9a
Sidnei <sidneisonic@hotmail.com>
2024-03-14 18:24:43 -0300

    Aula 47 exemplos praticos do uso do cleanup

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,16 +1,38 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export const Card = () => {
+    const [count, setCount] = useState(0);
+    
+
+    // useEffect(() => {
+    //     console.log("componentDidMount");
+
+    //     const intervalId = setInterval(() => {
+    //         setCount(count + 1);
+
+    //         console.log("Executei o count é:", count);
+    //     }, 1000);
+
+    //     return () => {
+    //         console.log("componentWillUnmount");
+    //         clearInterval(intervalId);
+    //     }
+    // }, [count])
+
     useEffect(() => {
-        console.log("componentDidMount");
-        return () => {
-            console.log("componentWillUnmount");
+        const handleWindowClick = () => {
+            console.log("Clicou na janela");
         }
+
+        window.addEventListener("click", handleWindowClick);
+
+        return () => window.removeEventListener("click", handleWindowClick);
     }, [])
 
     return (
         <div>
             <h1>Card</h1>
+            <h2>A quantidade é {count}</h2>
         </div>
     );
 
